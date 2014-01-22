bc2578e8e4ba349c05b62900e6fbc7ebb75fbf7c
gusgol <gustavo.goldhardt@gmail.com>
2014-01-22 19:21:51 -0200

    Added search form on clients

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,12 @@
 class Client < ActiveRecord::Base
 	has_many :animals, dependent: :destroy
 	has_many :services, dependent: :destroy
+
+	def self.search(search)
+		if search
+			find(:all, :conditions => ['name LIKE ?', "%#{search}%"], :order => 'name')
+  		else
+    		find(:all, :order => 'name')
+  		end
+	end
 end
